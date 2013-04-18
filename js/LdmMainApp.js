2611cb2eb2de0ea32693d4ec049106258ce4e1a4
Marek Gerhart <marek.gerhart@gooddata.com>
2013-04-18 12:46:10 +0200

    property editor enhancements - v.0.0.0.1.1.pre-alpha

--- js/LdmMainApp.js
+++ js/LdmMainApp.js
@@ -1,17 +1,49 @@
 function MainCtrl($scope) {
     $scope.datasets = [
-        { id: "dataset.person", title: "Person" },
-        { id: "dataset.department", title: "Department" }
+        {
+            id: "dataset.person",
+            title: "Person",
+            attributes: [
+                {
+                    id: "some.attribute1",
+                    title: "some attribute 1"
+                },
+                {
+                    id: "some.other.attribute",
+                    title: "some other attribute"
+                }
+            ],
+
+            facts: [
+                {
+                    id: "number.of.brothers",
+                    title: "number of brothers"
+                },
+                {
+                    id: "age",
+                    title: "age"
+                }
+            ]
+        },
+        {
+            id: "dataset.department",
+            title: "Department" }
     ];
+
+    $scope.selectedDataset = null;
 
     function uuid() {
         return 'xxxxxxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
     }
 
     $scope.addDataset = function(title) {
+        if (!title) {
+            return;
+        }
+
         $scope.newDatasetTitle = "";
         $scope.datasets.push({
             id: "dataset." + uuid(),
--- js/LdmPropertyEditor.js
+++ js/LdmPropertyEditor.js
@@ -2,7 +2,9 @@ angular.module('ldm', []).
     config(function($routeProvider) {
         $routeProvider.
             when('/', {controller: DatasetListCtrl, templateUrl: 'views/datasetList.html'}).
-            when('/edit/:datasetId', {controller: EditDatasetCtrl, templateUrl: 'views/datasetDetail.html'}).
+            when('/dataset/:datasetId', {controller: DatasetDetailsCtrl, templateUrl: 'views/datasetDetail.html'}).
+            when('/dataset/:datasetId/attribute/:attributeId', {controller: AttributeDetailsCtrl, templateUrl: 'views/attributeDetail.html'}).
+            when('/dataset/:datasetId/fact/:factId', {controller: FactDetailsCtrl, templateUrl: 'views/factDetail.html'}).
             otherwise({redirectTo: '/'});
     });
 
@@ -10,28 +12,30 @@ angular.module('ldm', []).
 function DatasetListCtrl($scope) {
 }
 
-function MainCtrl($scope) {
-    $scope.datasets = [
-        { id: "dataset.person", title: "Person" },
-        { id: "dataset.department", title: "Department" }
-    ];
+function DatasetDetailsCtrl($scope, $routeParams) {
+    $scope.$parent.selectedDataset = findDatasetById($scope.datasets, $routeParams.datasetId);
 }
 
-function EditDatasetCtrl($scope, $location, $routeParams, Project) {
+
+function AttributeDetailsCtrl($scope, $location, $routeParams) {
     var self = this;
 
-    Project.get({id: $routeParams.projectId}, function(project) {
-        self.original = project;
-        $scope.project = new Project(self.original);
-    });
-
-    $scope.isClean = function() {
-        return angular.equals(self.original, $scope.project);
+    if (!$scope.selectedDataset) {
+        $scope.$parent.selectedDataset = findDatasetById($scope.datasets, $routeParams.datasetId);
     }
 
-    $scope.destroy = function() {
+    $scope.attribute = findInDatasetById($scope.selectedDataset, 'attributes', $routeParams.attributeId);
+
+
+    $scope.isClean = function() {
+//        return angular.equals(self.original, $scope.project);
+        return true;
+    }
+
+    // todo remove attribuge
+    $scope.delete = function() {
         self.original.destroy(function() {
-            $location.path('/list');
+            $location.path('/dataset/' + self.$scope.selectedDataset.id);
         });
     };
 
@@ -40,4 +44,66 @@ function EditDatasetCtrl($scope, $location, $routeParams, Project) {
             $location.path('/');
         });
     };
+}
+
+function FactDetailsCtrl($scope, $location, $routeParams) {
+    var self = this;
+
+    if (!$scope.selectedDataset) {
+        $scope.$parent.selectedDataset = findDatasetById($scope.datasets, $routeParams.datasetId);
+    }
+
+    $scope.attribute = findInDatasetById($scope.selectedDataset, 'facts',  $routeParams.attributeId);
+
+
+    $scope.isClean = function() {
+//        return angular.equals(self.original, $scope.project);
+        return true;
+    }
+
+    // todo remove attribuge
+    $scope.delete = function() {
+        self.original.destroy(function() {
+            $location.path('/dataset/' + self.$scope.selectedDataset.id);
+        });
+    };
+
+    $scope.save = function() {
+        $scope.project.update(function() {
+            $location.path('/');
+        });
+    };
+}
+
+function findDatasetById(datasets, datasetId) {
+
+    if (!datasets) {
+        return null;
+    }
+
+    // better array looping ?
+    for (var i = 0; i < datasets.length; i++) {
+        if (datasets[i].id === datasetId) {
+            return datasets[i];
+        }
+    }
+    return null;
+}
+
+function findInDatasetById(dataset, propertyType, attributeId) {
+    var attributes;
+
+    if (!dataset || !dataset[propertyType]) {
+        return null;
+    }
+
+    attributes = dataset[propertyType];
+
+    // better array looping ?
+    for (var i = 0; i < dataset[propertyType].length; i++) {
+        if (attributes[i].id === attributeId) {
+            return attributes[i];
+        }
+    }
+    return null;
 }
