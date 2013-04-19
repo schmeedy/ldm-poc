bdc37a31b5e9cfd676d9c777ded904ad6124127f
Marek Gerhart <marek.gerhart@gooddata.com>
2013-04-19 18:06:06 +0200

    swap radiobutton and input when adding attributes/facts

--- js/model.js
+++ js/model.js
@@ -1,4 +1,6 @@
 angular.module('model', ['ngResource']).
     factory('LdmResource', function($resource) {
+
+        // angular bug, https://github.com/angular/angular.js/issues/1243 (double port)
         return $resource('http://10.151.1.204:8080:8080/gdc/projects/zgfs16g7iy7hkyeu0kkphblqfynjznw4/ldm/view/EIO5wrE7Igo2vHyvqGnFQlNImWVvXGAj/result');
     });
