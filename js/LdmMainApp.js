72d5f1a15cb77c155da4528cda432ea12554d5ef
Marek Gerhart <marek.gerhart@gooddata.com>
2013-04-19 20:34:59 +0200

    spinner added

--- js/LdmMainApp.js
+++ js/LdmMainApp.js
@@ -143,6 +143,26 @@ ldmApp.factory('Utils', function() {
 // main controller responsible for the whole app
 function MainCtrl($scope, $location, $timeout, Utils, LdmMainResource, LdmPollResource, LdmResultResource) {
 
+    var opts = {
+        lines: 10, // The number of lines to draw
+        length: 20, // The length of each line
+        width: 5, // The line thickness
+        radius: 30, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 24, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb
+        speed: 1.2, // Rounds per second
+        trail: 50, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9 // The z-index (defaults to 2000000000)
+    };
+    var spinner = new Spinner(opts).spin();
+    $("#top-bar").append(spinner.el);
+
+
     $scope.datasets = [];
 
     $scope.project = {id: 'zgfs16g7iy7hkyeu0kkphblqfynjznw4'};
@@ -165,6 +185,7 @@ function MainCtrl($scope, $location, $timeout, Utils, LdmMainResource, LdmPollRe
                         id: resultData.ldm.links.project.replace("/gdc/projects/", ""),
                         title: resultData.ldm.projectMeta.title
                     };
+                    spinner.stop();
                 });
             });
         }
