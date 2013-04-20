f69be70f6639d534e4028ff2929db25846279e4d
Marek Gerhart <marek.gerhart@gooddata.com>
2013-04-20 21:41:59 +0200

    inline editing of dataset title and attribute title

--- js/directives.js
+++ js/directives.js
@@ -17,20 +17,19 @@ angular.module('deleteButton', []).directive('buttonDelete', function($compile, 
     };
 });
 
-angular.module('buttonsRadio', [])
-    .directive('buttonsRadio', function() {
-        return {
-            restrict: 'E',
-            scope: { model: '=', options: '='},
-            controller: function($scope) {
-                $scope.activate = function(option) {
-                    $scope.model = option;
-                };
-            },
-            template: "<button type='button' class='btn' " +
-                "ng-class='{active: option.value == model}'" +
-                "ng-repeat='option in options' " +
-                "ng-click='activate(option.value)'>{{option.title}} " +
-                "</button>"
-        };
-    });
+angular.module('buttonsRadio', []).directive('buttonsRadio', function() {
+    return {
+        restrict: 'E',
+        scope: { model: '=', options: '='},
+        controller: function($scope) {
+            $scope.activate = function(option) {
+                $scope.model = option;
+            };
+        },
+        template: "<button type='button' class='btn' " +
+            "ng-class='{active: option.value == model}'" +
+            "ng-repeat='option in options' " +
+            "ng-click='activate(option.value)'>{{option.title}} " +
+            "</button>"
+    };
+});
